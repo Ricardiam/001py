dfcfef1d13b0ea800a2673f2b3e7490d93789215
Ricky <93832900+Ricardiam@users.noreply.github.com>
2025-04-04 17:49:44 -0500

    001

---
+++ p.py
@@ -0,0 +1,7 @@
+a = int(input("Ingresa un número: "))  # Solicita un número al usuario
+if a > 5:
+    print("El número ingresado es mayor a 5, no se ejecutará el bucle.")
+else:
+    while a <= 5:
+        print(a)  # Imprime el valor actual
+        a = a + 1  # Incrementa en 1
